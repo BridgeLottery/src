20dbe396eef04b92daca47714b24fc06c9d940ad
larfef <120338027+larfef@users.noreply.github.com>
2023-08-05 01:39:47 +0200

    Create depositor.sol
    This contract will store the senders addresses and amounts of incoming transaction in the OP bridge

---
+++ depositor.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DepositorAddress 
+{
+
+    mapping(address => uint256) public addressToInteger;
+    mapping(uint256 => address) public integerToAddress;
+
+    event IntegerUpdated(address indexed _address, uint256 _newValue);
+    event AddressAdded(address);
+
+    function addAddress(address _address, uint256 _initialValue) external 
+    {
+      require(addressToInteger[_address] == 0, "Address already exists in mapping");
+      addressToInteger[_address] = _initialValue;
+      integerToAddress[index] = _address;
+      index++;
+      emit AddressAdded(_address);
+    }
+}
