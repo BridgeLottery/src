6a5809d5c44c879afb26c342cd0e3d39e3da2ead
larfef <120338027+larfef@users.noreply.github.com>
2023-08-05 12:23:31 +0200

    Create txLogs.js
    This script is design to listen to incoming pending transaction in a smart contract

---
+++ txLogs.js
@@ -0,0 +1,31 @@
+console.log("Loaded")
+
+let web3 = new Web3("wss://mainnet.infura.io/ws/v3/5b7465b67f684d79be59b0289fbb6ac9")
+
+let hash = 0;
+
+let subscription = web3.eth
+  .subscribe(
+    "logs",
+    {
+      address: "0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1",
+    },
+    function (error, result) {
+      if (!error) {
+        web3.eth.getTransaction(result.transactionHash, (err, tx) => {
+            if (tx) {
+              if (hash !== tx.hash)
+              {
+                hash = tx.hash
+                console.log("Gotten transaction")
+				        console.log("Dépôt :", tx.value)
+				        console.log("Expéditeur :", tx.from)
+              }
+            }
+        })
+      }
+    }
+  )
+  .on("data", function (log) {
+    console.log(log);
+  });
