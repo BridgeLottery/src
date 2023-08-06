148b91d1b3e2ae9a4779bddc182fa4baf5c83f0c
larfef <120338027+larfef@users.noreply.github.com>
2023-08-06 23:55:28 +0000

    Update txLogs.js
    Add call to depositor.sol

--- txLogs.js
+++ txLogs.js
@@ -6,32 +6,47 @@ let hash = 0;
 
 const subscriptionDurationInSeconds = 10;
 
-let subscription = web3.eth
-  .subscribe(
+let subscription = web3.eth.subscribe(
     "logs",
     {
-      address: "0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1",
+        address: "0x636Af16bf2f682dD3109e60102b8E1A089FedAa8",
     },
-    function (error, result) {
-      if (!error) {
-        web3.eth.getTransaction(result.transactionHash, (err, tx) => {
-            if (tx) {
-              if (hash !== tx.hash)
-              {
-                hash = tx.hash
-                console.log("Gotten transaction")
-				        console.log("Dépôt :", tx.value)
-				        console.log("Expéditeur :", tx.from)
-              }
-            }
-        })
-      }
-    }
-  )
-  .on("data", function (log) {
-    console.log(log);
-  });
+     async function (error, result) {
+        if (!error) {
+            try 
+            {
+                const tx = await web3.eth.getTransaction(result.transactionHash); 
+                if (tx && tx.value) 
+                {
+                    if (hash !== tx.hash) 
+                    {
+                        hash = tx.hash;
+                        console.log("Gotten transaction");
+                        console.log("Dépôt:", tx.value);
+                        console.log("Expéditeur:", tx.from);
 
+                        const txObject = 
+                        {
+                            nonce: nonce,
+                            to: depositorContractAddress,
+                            gasLimit: web3.utils.toHex(300000),
+                            gasPrice: web3.utils.toHex(300000),
+                            value: "0x0",
+                            data: depositorContract.methods.addAddress(tx.from, tx.value).encodeABI(),
+                        };
+                        const signedTx = await web3.eth.accounts.signTransaction(txObject, privateKey);
+                        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+                        console.log("Transaction Receipt:", receipt);
+                    } 
+        } 
+            catch (error) 
+            {
+                console.error("Error:", error);
+            }
+        }
+    }
+);
 setTimeout(() => {
     if (subscription) {
         subscription.unsubscribe((error, success) => {
