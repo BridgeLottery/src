3f6cee7c0f11166e0ae461484f514e1f1c821caa
ksainte <129754308+ksainte@users.noreply.github.com>
2023-08-09 21:08:39 +0000

    Update depositor.sol :Now includes OnlyOwner

--- depositor.sol
+++ depositor.sol
@@ -1,26 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract DepositorAddress 
-{
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DepositorAddress is Ownable{
+
+    uint256 public index = 0;
 
     mapping(address => uint256) public addressToInteger;
     mapping(uint256 => address) public integerToAddress;
 
     event IntegerUpdated(address indexed _address, uint256 _newValue);
     event AddressAdded(address);
-
-    function setInteger(address _address, uint256 _value) external {
-        addressToInteger[_address] += _value;
-        emit IntegerUpdated(_address, _value);
+    
+    function addAddress(address _address, uint256 _initialValue) public onlyOwner {        
+        require(addressToInteger[_address] == 0, "Address already exists in mapping");
+        addressToInteger[_address] = _initialValue;
+        integerToAddress[index] = _address;
+        index++;
+        emit AddressAdded(_address);
     }
-
-    function addAddress(address _address, uint256 _initialValue) external 
-    {
-      require(addressToInteger[_address] == 0, "Address already exists in mapping");
-      addressToInteger[_address] = _initialValue;
-      integerToAddress[index] = _address;
-      index++;
-      emit AddressAdded(_address);
     }
-}
