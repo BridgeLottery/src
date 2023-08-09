af0aeea5caa078b1933160666c6bb813d26d1fc8
larfef <120338027+larfef@users.noreply.github.com>
2023-08-09 17:24:35 +0000

    Update txLogs.js
    Add a function to deploy smart contract on sepolia

--- txLogs.js
+++ txLogs.js
@@ -1,3 +1,4 @@
+//Imports
 const axios = require('axios');
 const depositorABI = require('./ABIjson/depositorAbi.json');
 const { ethers } = require('ethers');
@@ -5,36 +6,71 @@ const fs = require('fs');
 const { EAS, SchemaEncoder } = require("@ethereum-attestation-service/eas-sdk");
 const randomNumberABI = require('./ABIjson/randomNumberAbi.json');
 const Web3 = require('web3');
-
-// Read and parse the config.json file
 const configPath = './config.json';
+
+//Read and parse the config.json file
 const configFile = fs.readFileSync(configPath, 'utf-8');
 const config = JSON.parse(configFile);
-
+let depositorContract;
+//Connection to goerli the network in our case
 let web3Url = config.web3Socket.replace('$web3Key', config.web3Key);
-let web3 = new Web3(web3Url);
+let web3Sub = new Web3(web3Url);
+//connection to sepolia network
+web3Url = config.yourEndpointUrl.replace('$yourEndpointKey' , config.yourEndpointKey)
+let web3 = new Web3(web3Url)
 
+//Must be the address who deploy the contracts
 web3.eth.defaultAccount = config.fromAddress;
-const depositorContract = new web3.eth.Contract(depositorABI, config.depositorContractAddress);
-const subscriptionDurationInSeconds = 100;
 
-let hash = 0;
-let gasPriceRequested = false;
-let suggestedGasPriceWei = 0;
-let nonce = 0;
+//Tx hash
+let hash;
 
+//Transactions count
+let nonce;
+
+//Gas price
+let estimatedGas;
+let suggestedGasPrice;
+
+//Subscription duration
+const subscriptionDurationInSeconds = 120;
+
+//Deploy a new contract
+const deploy = async () => {
+    const nonce = await web3.eth.getTransactionCount(config.fromAddress);
+    
+    const deployTransaction = {
+        from: config.fromAddress,
+        data: config.contractBytecode,
+        nonce: nonce,
+        gas: web3.utils.toHex(5000000), // Adjust the gas limit as needed
+        gasPrice: await web3.eth.getGasPrice(), // Adjust gas price as needed
+    };
+    
+    const signedTransaction = await web3.eth.accounts.signTransaction(deployTransaction, config.privateKey);
+    const contract = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+    console.log('Contract deployed at:', contract.contractAddress);
+    return contract.contractAddress; // Return the deployed contract address
+};
+
+deploy().then(deployedContractAddress => {
+    // Create a contract instance
+    depositorContract = new web3.eth.Contract(depositorABI, deployedContractAddress);
+    main();
+}).catch(error => {
+    console.error('Error deploying contract:', error);
+});
+
+//Sign and send the transaction to the network
 async function sendTransaction(txObject) {
     let signedTx = await web3.eth.accounts.signTransaction(txObject, config.privateKey);
-    console.log("test")
     let receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-    return receipt;
+    console.log("Transaction Receipt:", receipt);
 }
 
 async function main() {
-    nonce = await web3.eth.getTransactionCount(config.fromAddress);
-    let lastGasPriceWei = 0;
-
-    const subscription = web3.eth.subscribe(
+    //Subscribe to contract event
+    const subscription = web3Sub.eth.subscribe(
         "logs",
         {
             address: config.eventAddress
@@ -42,45 +78,48 @@ async function main() {
         async function (error, result) {
             if (!error) {
                 try {
-                    let tx = await web3.eth.getTransaction(result.transactionHash);
+                    //Recover data from the transaction
+                    console.log("test")
+                    let tx = await web3Sub.eth.getTransaction(result.transactionHash);
+                    //Verify that each transaction is used once
+                    //Verify that the transaction have a non-zero value
                     if (tx && tx.value && hash !== tx.hash) {
                         hash = tx.hash
                         console.log("Got transaction");
                         console.log("Dépôt:", tx.value);
                         console.log("Expéditeur:", tx.from);
-                        if (!gasPriceRequested) {
-                            suggestedGasPriceWei = await web3.eth.getGasPrice();
-                            gasPriceRequested = true;
-                        }
-                        let estimatedGas = await depositorContract.methods.addAddress(tx.from, tx.value).estimateGas();
 
+                        //Get the gas price
+                        suggestedGasPrice = await web3.eth.getGasPrice();
+                        console.log(suggestedGasPrice)
+                        //Get the estimated gas 
+                        estimatedGas = await depositorContract.methods.addAddress(tx.from, tx.value).estimateGas();
+                        //Recover the current transactions count of the input address
+                        nonce = await web3.eth.getTransactionCount(config.fromAddress);
+
+                        //Set up the txObject
                         let txObject = {
                             nonce: nonce,
-                            to: config.depositorContractAddress,
+                            to: depositorContract.options.address,
                             gasLimit: web3.utils.toHex(estimatedGas),
-                            gasPrice: web3.utils.toHex(suggestedGasPriceWei),
+                            gasPrice: web3.utils.toHex(suggestedGasPrice),
                             value: "0x0",
                             data: depositorContract.methods.addAddress(tx.from, tx.value).encodeABI(),
                         };
-                        nonce++;
-
-                        if (suggestedGasPriceWei < lastGasPriceWei) {
-                            suggestedGasPriceWei = lastGasPriceWei;
-                        } else {
-                            lastGasPriceWei = suggestedGasPriceWei;
+                        //nonce++;
+                        await sendTransaction(txObject);
                         }
-
-                        let receipt = await sendTransaction(txObject);
-                        console.log("Transaction Receipt:", receipt);
                     }
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        }
-    );
+                    catch (error) {
+                        console.log(error);}
 
-    setTimeout(async () => {
+                }
+                else {
+                    console.log(error);}
+            }
+            );
+//Set the duration of the subscription
+setTimeout(async () => {
         if (subscription) {
             subscription.unsubscribe((error, success) => {
                 if (success) {
@@ -100,19 +139,21 @@ async function getRandomNumber() {
         web3Url = config.yourEndpointUrl.replace('$yourEndpointKey' , config.yourEndpointKey)
         web3 = new Web3(web3Url);
         const randomNumberContract = new web3.eth.Contract(randomNumberABI, config.randomNumberContractAddress);
-        const estimatedGas = await randomNumberContract.methods.requestRandomWords().estimateGas();
-        const gasPrice = await web3.eth.getGasPrice();
-        console.log(estimatedGas)
-        console.log(gasPrice)
+        estimatedGas = await randomNumberContract.methods.requestRandomWords().estimateGas();
+        gasPrice = await web3.eth.getGasPrice();
         nonce = await web3.eth.getTransactionCount(config.fromAddress);
+        
+        console.log("estimatedGas", estimatedGas)
+        console.log("Gas price :", gasPrice)
         console.log("nonce", nonce)
             
         let txObject = {
             nonce: nonce,
             to: config.randomNumberContractAddress,
-            //Adjust if needed
-            gasLimit: web3.utils.toHex(estimatedGas * 10),
-            gasPrice: web3.utils.toHex(gasPrice * 10),
+
+            //@dev Adjust config.gasLimitModifier and config.gasPriceModifier if needed
+            gasLimit: web3.utils.toHex(estimatedGas * config.gasLimitModifier),
+            gasPrice: web3.utils.toHex(gasPrice * config.gasPriceModifier),
             value: "0x0",
             data: randomNumberContract.methods.requestRandomWords().encodeABI(),
         };
@@ -121,7 +162,6 @@ async function getRandomNumber() {
         console.log(signedTx)
         let receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 
-        //let receipt = await sendTransaction(txObject);
         console.log("Random Number Transaction Receipt:", receipt);
         let RequestID = await randomNumberContract.methods.lastRequestId().call()
         console.log(RequestID)
@@ -171,4 +211,3 @@ async function createAttestation(winner) {
 
     console.log("New attestation UID:", newAttestationUID);
 }
-main();
